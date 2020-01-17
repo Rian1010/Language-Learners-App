a37d29ab44050bfde9a6a503648d3f759c6d3ece
Rian Sunthbocus <rian@Rians-Air.local>
2020-01-17 20:06:14 +0100

    Tried to implement an animation using JavaScript and then tried to add jQuery.

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.HP-BG-grey-sec').click(function() {
+        $('.particle').hide();
+    });
+});
+// let particlesSection = document.getElementById('HPsection5');
+// let particles = document.getElementById('particle');
+// const colours = ['blue', 'orange', 'green', 'yellow'];
+// let pos = 0;
+// let i;
+
+
+// function setMouseCoordinates(e) {
+//     let pageX = e.screenX - e.clientX;
+//     let x = pageX + elem.offsetLeft;
+    
+
+//     let travellingTime = setInterval(moveParticle, 30)
+
+//     function moveParticle() {
+//         if (pos >= x) {
+//             pos = 0;
+//         } else {
+//             pos++;
+//             particles.style.right = pos + "px";
+//         }
+//     }
+// }
