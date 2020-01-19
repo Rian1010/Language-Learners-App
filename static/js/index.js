038b34ab045e4b7dc5f1cfc9d9f47dc634cfba08
Rian Sunthbocus <rian@Rians-Air.local>
2020-01-20 00:10:49 +0100

    Worked on the JavaScript animation for the homepage to select random background colours for the particles.

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,63 @@
 let particlesSection = document.getElementById('hp-section5');
 let particles = document.querySelectorAll('.particles');
 let particlesID = document.getElementById('particles')
-const colours = ['blue', 'orange', 'green', 'yellow'];
+const colours = ['blue', 'orange', 'green', 'yellow', 'purple', 'pink', 'black', 'turquoise', 'red'];
+let colourPicker = Math.floor(Math.random() * colours.length);
 let pos = 0;
-let i;
 
 let sectionwWidth = particlesSection.offsetWidth;
 
 let timeOut = 1000;
 setTimeout(function () {
-    let travellingTime = setInterval(moveParticle, Math.floor(Math.random() * 10+10))
+    let travellingTime = setInterval(moveParticle, Math.floor(Math.random() * 10 + 10))
+    console.log(travellingTime);
 
     function moveParticle() {
-        particles.forEach(particle => {
+        for (let i = 0; i < particles.length; i++) {
             if (pos >= sectionwWidth) {
                 pos = 0;
             } else {
                 pos++;
-                particle.style.backgroundColor = colours[0];
-                particle.style.display = "block";
-                particle.style.right = pos + "px";
-                particle.style.top = "10px";
+                particles[i].style.backgroundColor = colours[colourPicker];
+                particles[i].style.position = "absolute";
+                particles[i].style.display = "inline-block";
+                particles[i].style.right = pos + "px"
+                particles[i].style.top = "10px";
             }
-        });
+        }
     }
 }, timeOut);
+
+// let timeOut = 1000;
+// setTimeout(function () {
+//     let travellingTime = setInterval(moveParticle, Math.floor(Math.random() * 10+10))
+
+//     function moveParticle() {
+//         particles.forEach(particle => {
+//             if (pos >= sectionwWidth) {
+//                 pos = 0;
+//             } else {
+//                 pos++;
+//                 particle.style.backgroundColor = colours[0];
+//                 particle.style.display = "block";
+//                 particle.style.right = pos + "px";
+//                 particle.style.top = "10px";
+//             }
+//         });
+//     }
+// }, timeOut);
+
+
+
+
+
+
+
+// particles[i].addEventListener.one("mouseover", hoverFunc);
+
+// function hoverFunc() {
+//     if (true) {
+//         pos--;
+//         particles[i].style.left = pos + "px";
+//     }
+// }
