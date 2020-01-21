4488b6fedd56e92694a2dd7134e06b9064c7adba
Rian1010 <rtegally1098@gmail.com>
2020-01-22 00:50:48 +0100

    Refactored the code for the  animation

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,34 +1,36 @@
 $(document).ready(function () {
-    $(".particles-area").append("<div class='col-12 single-particle'></div>");
-    $(".single-particle").append("<div class='particles'></div>");
-});
-
-let particlesSection = document.getElementById('hp-section4');
-let particles = document.querySelectorAll('.particles');
-let sectionHeight = particlesSection.clientHeight;
-let sectionWidth = particlesSection.clientWidth - 10;
-const colours = ['blue', 'orange', 'green', 'yellow', 'purple', 'pink', 'black', 'turquoise', 'red'];
-
-particles.forEach(particle => {
-    let randomInterval = Math.floor((Math.random() * 10)) + 1;
-    let currentTop = Math.floor(Math.random() * sectionHeight) / 1.2;
-    let currentRight = Math.floor(Math.random() * sectionWidth) + 1;
-    let colourPicker = Math.floor(Math.random() * colours.length);
-    let animationSpeed = Math.random() + 1;
-
-    setInterval(moveParticle, randomInterval);
-
-    function moveParticle() {
-        particle.style.backgroundColor = colours[colourPicker];
-        particle.style.top = currentTop + "px";
-        particle.style.right = currentRight + "px";
-        particle.style.animationDuration = animationSpeed + "s";
-        if (currentRight >= sectionWidth) {
-            currentRight = 10;
-        } else {
-            currentRight++;
-        }
+    for (let i = 0; i < 5; i++) {
+        $(".particles-area").append("<div class='col-12 single-particle'></div>");
+        $(".single-particle").append("<div class='particles'></div>");
     }
+    
+    let particlesSection = document.getElementById('hp-section4');
+    let particles = document.querySelectorAll('.particles');
+    let sectionHeight = particlesSection.clientHeight;
+    let sectionWidth = particlesSection.clientWidth - 10;
+    const colours = ['blue', 'orange', 'green', 'yellow', 'purple', 'pink', 'black', 'turquoise', 'red'];
+    
+    particles.forEach(particle => {
+        let randomInterval = Math.floor((Math.random() * 10)) + 1;
+        let currentTop = Math.floor(Math.random() * sectionHeight) / 1.2;
+        let currentRight = Math.floor(Math.random() * sectionWidth) + 1;
+        let colourPicker = Math.floor(Math.random() * colours.length);
+        let animationSpeed = Math.random() + 1;
+    
+        setInterval(moveParticle, randomInterval);
+    
+        function moveParticle() {
+            particle.style.backgroundColor = colours[colourPicker];
+            particle.style.top = currentTop + "px";
+            particle.style.right = currentRight + "px";
+            particle.style.animationDuration = animationSpeed + "s";
+            if (currentRight >= sectionWidth) {
+                currentRight = 10;
+            } else {
+                currentRight++;
+            }
+        }
+    });
 });
 
 
