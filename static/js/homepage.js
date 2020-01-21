e64ceac21648323cf4d5cf1be7163a8b2465b79f
Rian1010 <rtegally1098@gmail.com>
2020-01-21 23:37:53 +0100

    Trying to reduce some of the codes for the animation of the homepage

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,16 +1,20 @@
+$(document).ready(function () {
+    $(".particles-area").append("<div class='col-12 single-particle'></div>");
+    $(".single-particle").append("<div class='particles'></div>");
+});
+
 let particlesSection = document.getElementById('hp-section4');
 let particles = document.querySelectorAll('.particles');
 let sectionHeight = particlesSection.clientHeight;
-let sectionWidth = particlesSection.clientWidth-10;
+let sectionWidth = particlesSection.clientWidth - 10;
 const colours = ['blue', 'orange', 'green', 'yellow', 'purple', 'pink', 'black', 'turquoise', 'red'];
-
 
 particles.forEach(particle => {
     let randomInterval = Math.floor((Math.random() * 10)) + 1;
     let currentTop = Math.floor(Math.random() * sectionHeight) / 1.2;
     let currentRight = Math.floor(Math.random() * sectionWidth) + 1;
     let colourPicker = Math.floor(Math.random() * colours.length);
-    let animationSpeed = (Math.random() * 1) + 1;
+    let animationSpeed = Math.random() + 1;
 
     setInterval(moveParticle, randomInterval);
 
