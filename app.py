ba114fb071258594359bb11aa324b6ffc82e2ff4
Rian1010 <rtegally1098@gmail.com>
2020-02-15 21:12:09 +0100

    Enabled users to edit and add tasks to the task manager

--- app.py
+++ app.py
@@ -16,12 +16,23 @@ def index():
 
 @app.route('/task_manager')
 def task_manager():
-    return render_template("task-manager.html", tasks=mongo.db.tasks.find())
+    return render_template('task-manager.html', tasks=mongo.db.tasks.find())
 
 @app.route('/add_tasks')
 def add_tasks():
-    return render_template('add-tasks.html',
-    task_manager=mongo.db.task_manager.find())
+    return render_template('add-tasks.html', lesson=mongo.db.lesson.find(), task=mongo.db.task.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('task_manager'))
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit-task.html', task=the_task, categories=all_categories)
 
 @app.route('/lesson_one')
 def lesson_one():
