9bece2e4f8fdc40ca89c46d5a621cca24ffb4eb6
Rian1010 <rtegally1098@gmail.com>
2020-03-03 22:13:24 +0100

    Worked on the functionalities to add new lessons on the first lesson page and send me an email through the homepage

--- app.py
+++ app.py
@@ -10,17 +10,21 @@ app.config["MONGO_URI"] = 'mongodb+srv://Rian:j4JWQ1Ntzc9u0U7m@myfirstcluster-ge
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/task_manager')
 def task_manager():
     return render_template('task-manager.html', tasks=mongo.db.tasks.find())
 
+
 @app.route('/add_tasks')
 def add_tasks():
     return render_template('add-tasks.html', lessons=mongo.db.lessons.find(), task=mongo.db.task.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
@@ -28,112 +32,135 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('task_manager'))
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edit-task.html', task=the_task, lessons=mongo.db.lessons.find(), categories=all_categories)
 
+
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({"_id": ObjectId(task_id)})
     return redirect(url_for('task_manager'))
 
+
 @app.route('/community')
 def community():
-     posts = mongo.db.posts.find()
-     return render_template('community.html', posts=posts, lesson=mongo.db.lessons.find())
+    posts = mongo.db.posts.find()
+    return render_template('community.html', posts=posts, lesson=mongo.db.lessons.find())
+
 
 @app.route('/add_posts')
 def add_posts():
     return render_template('add-posts.html', posts=mongo.db.posts.find(), lessons=mongo.db.lessons.find())
 
+
 @app.route('/update_post/<post_id>', methods=['POST'])
 def update_post(post_id):
     posts = mongo.db.posts
     posts.update({'_id': ObjectId(post_id)},
-    {
-        'lesson_name':request.form.get('lesson_name'),
-        'post_content':request.form.get('post_content'),
+                 {
+        'lesson_name': request.form.get('lesson_name'),
+        'post_content': request.form.get('post_content'),
     }
     )
     return redirect(url_for('community'))
+
 
 @app.route('/insert_post', methods=['POST'])
 def insert_post():
     posts = mongo.db.posts
     posts.insert_one(request.form.to_dict())
     return redirect(url_for('community'))
-    
+
+
 @app.route('/edit_post/<post_id>')
 def edit_post(post_id):
     the_post = mongo.db.posts.find_one({"_id": ObjectId(post_id)})
     return render_template('edit-post.html', posts=the_post, lessons=mongo.db.lessons.find())
+
 
 @app.route('/delete_post/<post_id>')
 def delete_post(post_id):
     mongo.db.posts.remove({"_id": ObjectId(post_id)})
     return redirect(url_for('community'))
 
+
 @app.route('/lesson_one')
 def lesson_one():
     add_lesson1 = mongo.db.addLesson1.find()
     return render_template('lesson-one.html', add_lesson1=add_lesson1)
 
+
 @app.route('/add_new_lesson1')
 def add_new_lesson1():
     return render_template('add-lesson-1.html', add_lesson1=mongo.db.addLesson1.find())
+
 
 @app.route('/update_lesson1/<addLesson1_id>', methods=['POST'])
 def update_lesson1(addLesson1_id):
     add_lesson1 = mongo.db.addLesson1
     add_lesson1.update({'_id': ObjectId(addLesson1_id)},
     {
-        'email1':request.form.get('email1'),
-        'lesson1_content':request.form.get('lesson1_content'),
+        'heading1': request.form.get('heading1'),
+        'email1': request.form.get('email1'),
+        'lesson1_content': request.form.get('lesson1_content'),
     }
     )
     return redirect(url_for('lesson_one'))
+
 
 @app.route('/insert_lesson1', methods=['POST'])
 def insert_lesson1():
     add_lesson1 = mongo.db.addLesson1
     add_lesson1.insert_one(request.form.to_dict())
     return redirect(url_for('lesson_one'))
-    
+
+
 @app.route('/edit_lesson1/<addLesson1_id>')
 def edit_lesson1(addLesson1_id):
-    add_lesson1 = mongo.db.addLesson1.find_one({"_id": ObjectId(addLesson1_id)})
-    return render_template('edit-lesson1.html', add_lesson1=add_lesson1)
+    add_lesson1 = mongo.db.addLesson1.find_one(
+        {"_id": ObjectId(addLesson1_id)})
+    return render_template('edit-lesson-1.html', add_lesson1=add_lesson1)
+
 
 @app.route('/delete_lesson1/<addLesson1_id>')
 def delete_lesson1(addLesson1_id):
     mongo.db.addLesson1.remove({"_id": ObjectId(addLesson1_id)})
     return redirect(url_for('lesson_one'))
 
+
 @app.route('/lesson_two')
 def lesson_two():
     return render_template('lesson-two.html')
+
 
 @app.route('/lesson_three')
 def lesson_three():
     return render_template('lesson-three.html')
 
+
 @app.route('/lesson_four')
 def lesson_four():
     return render_template('lesson-four.html')
+
 
 @app.route('/lesson_five')
 def lesson_five():
     return render_template('lesson-five.html')
 
+
 @app.route('/lesson_six')
 def lesson_six():
     return render_template('lesson-six.html')
 
+
 @app.route('/advanced_lesson')
 def advanced_lesson():
     return render_template('advanced-lesson.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
---
+++ static/js/contact.js
@@ -0,0 +1,54 @@
+// All of the code below is copied from my previous project, as I wanted it to do the same things
+// There are only slight changes to the code of the previous project because I am using different names and ID names 
+function sendMail(name, email, message) {
+    // Records a user's name, email address and message for the receiver to see this information in an email
+    emailjs.send("gmail", "first_template", {
+            "from_name": name,
+            "from_email": email,
+            "project_request": message,
+        })
+        // Let users know, if the message was successfully sent or not
+        .then(
+            // Function that lets users know, if the message was successfully sent
+            function(response) {
+                let form = document.getElementById("message_response");
+                let replacement = form.innerHTML = "<h2>Thank you for your message! It has been sent!</h2>";
+            },
+            // Function that lets users know, if the message was notsuccessfully sent
+            function(error) {
+                let form = document.getElementById("message_response");
+
+                form.innerHTML = "<h2>There has been an error sending the message.</h2>";
+            });
+        return false;
+}
+
+window.onload = function() {
+    const submitBtn = document.querySelector('#submit');
+    const nameBox = document.querySelector('#name');
+    const emailBox = document.querySelector('#email');
+    const MsgBox = document.querySelector('#message');
+
+    submitBtn.addEventListener('click', sendMsg);
+
+    function sendMsg() {
+        let nameContent = nameBox.value;
+        let emailContent = emailBox.value;
+        let MsgContent = MsgBox.value;
+
+        // Alerts pop up, if a required field has not been filled in, before pressing on the submit
+        if (nameContent === '') {
+            alert('Please fill in the input field for your name.');
+        }
+        else if (emailContent === '') {
+            alert('Please fill in the input field for your email address.');
+        }
+        else if (MsgContent === '') {
+            alert('Please fill in the input field for your message.');
+        }
+        // The sendMail function gets called, if each of the required fields on the page have been filled in
+        else {
+            sendMail(nameContent, emailContent, MsgContent);
+        }
+    }
+}
